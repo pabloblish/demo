185c08343ba4bb77faba781d57698893307f51fe
pabloblish <40511757+pabloblish@users.noreply.github.com>
2018-07-16 08:52:22 -0500

    Create loadbalance.tf

---
+++ loadbalance.tf
@@ -0,0 +1,28 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE THE CLASIC LOAD BALANCER
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_elb" "test" {
+  name               = "foobar-terraform-elb"
+  availability_zones = ["us-west-1a", "us-west-1b"]
+  security_groups        = ["${aws_security_group.load_balancer.id}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 5
+    unhealthy_threshold = 5
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  tags {
+    Name = "foobar-terraform-elb.test"
+  }
+}
