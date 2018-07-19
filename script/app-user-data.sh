046084e54207c25e6affc9f050f0aab64e8e76f2
pabloblish <40511757+pabloblish@users.noreply.github.com>
2018-07-18 20:22:23 -0500

    Update app-user-data.sh

--- script/app-user-data.sh
+++ script/app-user-data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 yum update -y
-yum install -y httpd24 php70 mysql56-server php70-mysqlnd
-yum install stress -y
-cp -p /var/www/noindex/index.html /var/www/html/index.html
+#yum install -y httpd24 php70 mysql56-server php70-mysqlnd
+yum install httpd -y
+#cp -p /var/www/noindex/index.html /var/www/html/index.html
+echo "DR Environment" >  /var/www/html/index.html
 service httpd start
 service httpd enable
